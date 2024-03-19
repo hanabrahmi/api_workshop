951ebea4a47937a5f8c634b9a069b5b333d78919
hana brahmi <brahmihana7@gmail.com>
2024-03-20 00:27:25 +0100

    api workshop

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import UserList from './UserList'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>User information </h1>
+     <UserList/>
+    </div>
+  );
+}
+
+export default App;
